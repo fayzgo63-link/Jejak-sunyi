f4899b973b792afcf240b75a7e462c1efca8a8a0
fayzgo63-link <fayzgo63@gmail.com>
2026-07-25 12:40:09 +0700

    Fix formatting in hening.py banner section

--- hening.py
+++ hening.py
@@ -101,11 +101,11 @@ banner = f"""
 {Fore.WHITE}     │││╰────╯│    ││╭────╯││╰─╯╭╯ {Fore.CYAN}││      │    │││   │││    ││││
 {Fore.WHITE}     │││╭─────╯    │││────╮││╭─╮╰╮ {Fore.CYAN}╰────╮╮ │    │││   │││ ───╯│││
 {Fore.WHITE}╰────╯ ╰╰─────     ││╰────╯╯╰╯  ─╯ {Fore.CYAN} ────╯╯ ╰────╯ ╰   ╰╯╰────╮│ ╯
-{Fore.WHITE}                ╰────╯                 {Fore.CYAN}                  ────╯╯
+{Fore.WHITE}              ╰────╯                   {Fore.CYAN}                  ────╯╯
 {Fore.CYAN}
 {Fore.WHITE}⬣  Datang tidak untuk mencari nama, hilang bukan untuk dikenang  ⬣
 {Fore.WHITE}⬣         biarkan semua mengalir tanpa beban [anonimous]         ⬣
-{Fore.WHITE}⬣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⬣
+{Fore.WHITE}⬣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⬣
 """
 print(banner)
 host = ""
