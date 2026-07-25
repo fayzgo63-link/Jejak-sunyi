8062745d3996ca004f0d5bb207d8fbfeb3c603f3
fayzgo63-link <fayzgo63@gmail.com>
2026-07-25 10:54:10 +0700

    Update hening.py

--- hening.py
+++ hening.py
@@ -94,14 +94,13 @@ def runing_attack(ip,host,port_loader,time_loader,spam_loader,methods_loader,boo
 
 #DATA
 banner = f"""
-{Fore.YELLOW}╭─────────────────╮ ────────────────╯┌───────────────┐└───────────────┘│
-{Fore.YELLOW} ─────┐
-{Fore.YELLOW}─────┐│╭─────   ────┐ ────╮
-{Fore.YELLOW}     │││╭────╮│────┐│────╮│
-{Fore.YELLOW}     │││╰────╯│    ││╭────╯│
-{Fore.YELLOW}     │││╭─────╯    │││────╮│
-{Fore.YELLOW}╰────╯ ╰╰─────     ││    ││
-{Fore.YELLOW}              ╰────╯╰────╯
+{Fore.YELLOW} ─────┐                         ╮ 
+{Fore.YELLOW}─────┐│ ─────╮  ────┐  ────╮││
+{Fore.YELLOW}     │││╭────╮│────┐│ ────╮│││
+{Fore.YELLOW}     │││╰────╯│    ││╭────╯││╰╯╭╯
+{Fore.YELLOW}     │││╭─────╯    │││────╮││╭┐╰┐
+{Fore.YELLOW}╰────╯ ╰╰─────     │ │    │││ ╰┐╰┐
+{Fore.YELLOW}              ╰────╯  ╰────╯╰╯ ╰─
 """
 print(banner)
 host = ""
