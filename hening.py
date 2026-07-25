3503788276744397d96186fbd17051e47df61ba9
fayzgo63-link <fayzgo63@gmail.com>
2026-07-25 11:23:00 +0700

    Update hening.py

--- hening.py
+++ hening.py
@@ -96,11 +96,11 @@ def runing_attack(ip,host,port_loader,time_loader,spam_loader,methods_loader,boo
 banner = f"""
 {Fore.WHITE}                             ┐ 
 {Fore.WHITE} ─────┐                     ││  
-{Fore.WHITE}─────┐│ ─────╮  ────┐  ────╮││    ╮
+{Fore.WHITE}─────┐│ ──────╮ ────┐  ────╮││    ╮
 {Fore.WHITE}     │││╭────╮│────┐│ ────╮│││ ╭╯╭╯
 {Fore.WHITE}     │││╰────╯│    ││╭────╯││╰─╯╭╯
 {Fore.WHITE}     │││╭─────╯    │││────╮││╭─╮╰╮
-{Fore.WHITE}╰────╯ ╰╰─────     │ ╰────╯╰╯   ─╯  
+{Fore.WHITE}╰────╯ ╰╰─────     ││╰────╯╰╯   ─╯  
 {Fore.WHITE}              ╰────╯  
 """
 print(banner)
